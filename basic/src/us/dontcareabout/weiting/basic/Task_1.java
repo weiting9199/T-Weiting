7aa1c59a6ad513825b247f977a45148971bd41db
MontyPan <monty.pan@dontcareabout.us>
2019-06-26 10:39:49 +0800

    Task-1 空殼

---
+++ basic/src/us/dontcareabout/weiting/basic/Task_1.java
@@ -0,0 +1,35 @@
+package us.dontcareabout.weiting.basic;
+
+public class Task_1 {
+	/**
+	 * 在 level = 5 的 case 下要在 console 顯示：
+	 * <pre>
+	 *      1
+	 *     234
+	 *    56789
+	 *   0123456
+	 *  789012345
+	 * </pre>
+	 */
+	public static void main(String[] args) {
+		int level = 5;
+
+		version1(level);
+		System.out.println("================================");
+		version2(level);
+		System.out.println("================================");
+		version3(level);
+	}
+
+	private static void version1(int level) {
+		// TODO Auto-generated method stub
+	}
+
+	private static void version2(int level) {
+		// TODO Auto-generated method stub
+	}
+
+	private static void version3(int level) {
+		// TODO Auto-generated method stub
+	}
+}
